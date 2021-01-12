80b283c6ebf3ca9e31befc6fa3a62379602d2120
lau <lau.bannenberg@gmail.com>
2021-01-12 09:28:31 +0100

    Majority classifier + unit test

--- majority.py
+++ majority.py
@@ -1,9 +1,22 @@
+import numpy as np
 from model import AbstractModel
 
 
 class MajorityClassifier(AbstractModel):
+    def __init__(self):
+        super().__init__()
+        self.majority_label = None
+
     def fit(self, x_train, y_train):
-        raise NotImplementedError  # TODO Lau
+        """ Determine which is the majority label in the training set. """
+        majority = 0
+        majority_label = None
+        for label in np.unique(y_train):
+            if np.sum(y_train[y_train == label]) > majority:
+                majority = np.sum(y_train[y_train == label])
+                majority_label = label
+        self.majority_label = majority_label
 
     def predict(self, x_test):
-        raise NotImplementedError  # TODO Lau
+        """ Predict the trained majority label in all cases. """
+        return np.full(x_test.shape[0], self.majority_label)
--- majority_test.py
+++ majority_test.py
@@ -1,4 +1,20 @@
 # DO NOT CHANGE ANYTHING IN THIS FILE
-from majority import MajorityClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-raise NotImplementedError  # TODO Lau
+from majority import MajorityClassifier
+from util import generate_data
+
+
+np.random.seed(42)
+
+
+def test_majority():
+    X, y = generate_data()
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    clf = MajorityClassifier()
+    clf.fit(x_train, y_train)
+    pred_test = clf.predict(x_test)
+    score = accuracy_score(y_true=y_test, y_pred=pred_test)
+    assert score > 0.5
