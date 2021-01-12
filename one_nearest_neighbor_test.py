1ee9d68653d10e178c73c9ae8c7d3cb50eb39fb2
lau <lau.bannenberg@gmail.com>
2021-01-12 09:29:39 +0100

    Unit test for 1NN

--- one_nearest_neighbor_test.py
+++ one_nearest_neighbor_test.py
@@ -1,4 +1,20 @@
 # DO NOT CHANGE ANYTHING IN THIS FILE
-from one_nearest_neighbor import OneNearestNeighborClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-raise NotImplementedError  # TODO Lau
+from one_nearest_neighbor import OneNearestNeighborClassifier
+from util import generate_data
+
+
+np.random.seed(42)
+
+
+def test_one_nearest_neighbor():
+    X, y = generate_data()
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    clf = OneNearestNeighborClassifier()
+    clf.fit(x_train, y_train)
+    pred_test = clf.predict(x_test)
+    score = accuracy_score(y_true=y_test, y_pred=pred_test)
+    assert score > 0.9
